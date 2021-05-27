2499a5548b43f265f00d8f7f0c9167eba1499d9f
dbshah1212 <root@ubuntu16.cdsys.local>
2021-05-27 17:15:22 +0530

    TDL-13714: Added comment to explain the code

--- tap_yotpo/__init__.py
+++ tap_yotpo/__init__.py
@@ -44,6 +44,7 @@ def discover(ctx):
         mdata = metadata.to_map(mdata)
 
         for field_name in schema_dict['properties'].keys():
+            # Field except primary keys and book mark keys marked as available. 
             mdata = metadata.write(mdata, ('properties', field_name), 'inclusion',
                                    'automatic' if field_name in stream.pk_fields or field_name in stream.book_mark_keys else 'available')
 
