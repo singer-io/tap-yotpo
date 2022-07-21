899d3ee346d33c6b3a0bc50ab80c98dd21053e81
“rdeshmukh15” <redeshmukh@talend.com>
2022-07-21 12:07:12 +0000

    added test cases for TDL-19836 fix

--- tests/unittests/test_data_transforming.py
+++ tests/unittests/test_data_transforming.py
@@ -25,3 +25,41 @@ class TestDataTransforming(unittest.TestCase):
             transform_error = True
             
         self.assertEqual(transform_error, False, "Exception raised")
+
+    """
+    To test that data can transformable to schema.
+    Checking that in product Stream, url field should accept not accept any value but String or None values
+    """
+    def test_data_transform_for_products_stream(self):
+        ctx = Context({}, {})
+        ctx.catalog = discover(ctx)
+        schema = ctx.catalog.get_stream('products').schema.to_dict()
+        record = {"id": 37131285, "created_at": "2018-01-23T14:16:43.000000Z", "updated_at": "2021-05-24T09:23:43.000000Z", "average_score": 4.5, "total_reviews": 4.0, "external_product_id": "yotpo_site_reviews", "name": "https://stitchdatawearhouse.myshopify.com","url": NonStringDataType(), "description": "abcd", "product_specs": [], "category": {"id": 1, "name": "abcd"}, "images": []}
+
+        transform_error = False
+        
+        try:
+            result = transform(record, schema)
+        except:
+            transform_error = True
+            
+        self.assertEqual(transform_error, True, "Exception raised")
+
+    """
+    To test that data can transformable to schema.
+    Checking that in product Stream, url field should accept None value
+    """
+    def test_data_transform_for_products_stream(self):
+        ctx = Context({}, {})
+        ctx.catalog = discover(ctx)
+        schema = ctx.catalog.get_stream('products').schema.to_dict()
+        record = {"id": 37131285, "created_at": "2018-01-23T14:16:43.000000Z", "updated_at": "2021-05-24T09:23:43.000000Z", "average_score": 4.5, "total_reviews": 4.0, "external_product_id": "yotpo_site_reviews", "name": "https://stitchdatawearhouse.myshopify.com","url": None, "description": "abcd", "product_specs": [], "category": {"id": 1, "name": "abcd"}, "images": []}
+
+        transform_error = False
+        
+        try:
+            result = transform(record, schema)
+        except:
+            transform_error = True
+            
+        self.assertEqual(transform_error, False, "Exception raised")
