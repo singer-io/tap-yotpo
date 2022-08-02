5b567a42b54a6a9e3ebf9c52b14673d145076c6d
“rdeshmukh15” <redeshmukh@talend.com>
2022-08-02 14:12:34 +0000

    product_reviews stream implementation changes

--- tap_yotpo/streams/__init__.py
+++ tap_yotpo/streams/__init__.py
@@ -1,5 +1,7 @@
 from .products import Products
+from .product_reviews import ProductReviews
 
 STREAMS = {
-    Products.tap_stream_id:Products
+    Products.tap_stream_id:Products,
+    ProductReviews.tap_stream_id:ProductReviews
 }
--- tap_yotpo/streams/product_reviews.py
+++ tap_yotpo/streams/product_reviews.py
@@ -1,4 +1,9 @@
 from .abstracts import IncremetalStream
+from singer import metrics,write_record
+import singer
+import re
+
+LOGGER = singer.get_logger()
 
 
 class ProductReviews(IncremetalStream):
@@ -12,25 +17,53 @@ class ProductReviews(IncremetalStream):
     valid_replication_keys = ["created_at"]
     api_auth_version = "v1"
     config_start_key = "start_date"
-    url_endpoint = " https://api-cdn.yotpo.com/v1/widget/:APP_KEY/products/PRODUCT_ID/reviews.json"
+    url_endpoint = " https://api-cdn.yotpo.com/v1/widget/APP_KEY/products/PRODUCT_ID/reviews.json"
 
     def get_url_endpoint(self) -> str:
         """
         Returns a formated endpoint using the stream attributes
         """
-        for product_id in self.client.shared_product_ids:
-            return self.url_endpoint.replace("APP_KEY", self.client.config["api_key"]).replace("PRODUCT_ID",product_id)
+        return self.url_endpoint.replace("APP_KEY", self.client.config["api_key"])
 
     def get_records(self):
-        extraction_url =  self.get_url_endpoint()
-        call_next, page_size = True, 100
+        page_size = 100
         params,headers = {"page":1,"per_page": page_size,"sort": ["date", "time"],"direction": "asc"},{}
+        base_url =  self.get_url_endpoint()
+        for product_id in self.client.shared_product_ids:
+            call_next,params["page"] = True, 1
+            extraction_url =  base_url.replace("PRODUCT_ID", product_id)            
+            while call_next:
+                response =  self.client.get(extraction_url,params,headers,self.api_auth_version)
+                LOGGER.info("response...............: %s",response)
+                raw_records = response.get("response",{}).get(self.stream,[]) 
+                if not raw_records:
+                    call_next =  False
+                params["page"]+=1
+                yield from raw_records
 
-        while call_next:
-            response =  self.client.get(extraction_url,params,headers,self.api_auth_version)
-            raw_records = response.get(self.stream,[])
-            if not raw_records:
-                call_next =  False
-            params["page"]+=1
-            yield from raw_records
+    # def format_url(self):
+    #     for product_id in self.client.shared_product_ids:
+    #         if re.match("^[A-Za-z0-9_-]*$", product_id) is None:
+    #             LOGGER.warning(f"Product-id - {product_id} contains special character. Processing next product-id")
+    #             continue
+    #         extraction_url =  self.get_url_endpoint(product_id)
+    #         return extraction_url
+
+    def sync(self,state,schema,stream_metadata,transformer,write_records=True):
+        with metrics.record_counter(self.tap_stream_id) as counter:
+            for record in self.get_records():
+                transformed_record = transformer.transform(record, schema, stream_metadata)
+                if self.filter_record(record,state):
+                    write_record(self.tap_stream_id, transformed_record)
+                    counter.increment()
+        return state
+
+
+    def filter_record(self,record,state) ->bool:
+        """
+        Returns boolean if a record should be written
+        """
+        return True
+
+
 
