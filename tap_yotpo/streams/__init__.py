3851da95c7b0b9d5599fc2a846eec6c10dee4baf
VishalP <20889199+Vi6hal@users.noreply.github.com>
2022-08-03 10:20:39 +0530

    Changes: implemented paginated fulltable replication for unsubscribers stream

--- tap_yotpo/streams/__init__.py
+++ tap_yotpo/streams/__init__.py
@@ -1,7 +1,8 @@
 from .products import Products
 from .product_reviews import ProductReviews
-
+from .unsubscribers import Unsubscribers
 STREAMS = {
     Products.tap_stream_id:Products,
-    ProductReviews.tap_stream_id:ProductReviews
+    ProductReviews.tap_stream_id:ProductReviews,
+    Unsubscribers.tap_stream_id:Unsubscribers
 }
