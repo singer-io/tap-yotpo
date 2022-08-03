1c14c392c664bf14da777440c101e8e43e0d3c01
VishalP <20889199+Vi6hal@users.noreply.github.com>
2022-08-03 14:24:42 +0530

    updated bookmarking for reviews

--- tap_yotpo/streams/product_reviews.py
+++ tap_yotpo/streams/product_reviews.py
@@ -1,3 +1,4 @@
+from asyncio.log import logger
 from .abstracts import IncremetalStream
 from singer import metrics,write_record
 import singer
@@ -29,28 +30,38 @@ class ProductReviews(IncremetalStream):
         base_url,shared_product_ids =  self.get_url_endpoint(),Products(self.client).prefetch_product_ids()
         total_product_count = len(shared_product_ids)
         for current,product_id in enumerate(shared_product_ids,1):
-            call_next,params["page"] = True, 1
+            params["page"],self.sync_prod = 1,True
             extraction_url =  base_url.replace("PRODUCT_ID", product_id)
-            while call_next:
+            while True:
+                if not self.sync_prod:
+                    break
+                LOGGER.info("fetching page %s of product %s",params["page"],product_id)
                 response =  self.client.get(extraction_url,params,headers,self.api_auth_version)
                 raw_records = response.get("response",{}).get("reviews",[])
                 if not raw_records:
-                    call_next =  False
+                    break
+                try:
+                    domain_key = response.get("response",{}).get("products",[])[0].get("domain_key")
+                except IndexError as _:
+                    LOGGER.warning("%s domain key not found",current)
                 params["page"]+=1
-                yield from map(lambda _:(product_id,_),raw_records)
-            LOGGER.info("Extracted reviews for product %s of %s",current,total_product_count)
+                yield from map(lambda _:(domain_key,_),raw_records)
+            LOGGER.info("Extracted reviews for product %s of %s",current,total_product_count,)
+
 
     def sync(self,state,schema,stream_metadata,transformer):
         max_bookmark_value = singer.get_bookmark(state,self.tap_stream_id,self.replication_key,self.client.config[self.config_start_key])
         max_created_at = max_bookmark_value
         with metrics.record_counter(self.tap_stream_id) as counter:
-            for product_id,record in self.get_records():
-                record["domain_key"] = product_id
+            for domain_key,record in self.get_records():
+                record["domain_key"] = domain_key
                 transformed_record = transformer.transform(record, schema, stream_metadata)
                 if record[self.replication_key] >= max_bookmark_value:
                     write_record(self.tap_stream_id, transformed_record)
                     max_created_at = max(record[self.replication_key],max_created_at)
                     counter.increment()
+                else:
+                    self.sync_prod = False
         state = singer.write_bookmark(state, self.tap_stream_id, self.replication_key, max_created_at)
         singer.write_state(state)
         return state
