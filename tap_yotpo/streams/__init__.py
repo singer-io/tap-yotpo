e0f098ccd19caeb9109db7278cef1ab5bf5afd53
“rdeshmukh15” <redeshmukh@talend.com>
2022-08-03 15:47:18 +0000

    emails stream implementation changes

--- tap_yotpo/streams/__init__.py
+++ tap_yotpo/streams/__init__.py
@@ -2,10 +2,12 @@ from .products import Products
 from .product_reviews import ProductReviews
 from .unsubscribers import Unsubscribers
 from .reviews import Reviews
+from .emails import Emails
 
 STREAMS = {
     Products.tap_stream_id:Products,
     ProductReviews.tap_stream_id:ProductReviews,
     Unsubscribers.tap_stream_id:Unsubscribers,
-    Reviews.tap_stream_id:Reviews
+    Reviews.tap_stream_id:Reviews,
+    Emails.tap_stream_id:Emails
 }
---
+++ tap_yotpo/streams/emails.py
@@ -0,0 +1,56 @@
+from .abstracts import IncremetalStream,FullTableStream
+from singer import metrics,write_record,get_logger
+import singer
+#from .products import Products
+import pendulum
+LOGGER = singer.get_logger()
+
+
+class Emails(FullTableStream):
+    """
+    class for emails stream
+    """
+    stream = "emails"
+    tap_stream_id = "emails"
+    key_properties = ["email_address","email_sent_timestamp"]
+    replication_key = "email_sent_timestamp"
+    valid_replication_keys = ["email_sent_timestamp"]
+    api_auth_version = "v1"
+    config_start_key = "start_date"
+    url_endpoint = "https://api.yotpo.com/analytics/v1/emails/APP_KEY/export/raw_data?token=UTOKEN"
+
+    def get_url_endpoint(self) -> str:
+        """
+        Returns a formated endpoint using the stream attributes
+        """
+        return self.url_endpoint.replace("APP_KEY", self.client.config["api_key"]).replace("UTOKEN",self.client._get_auth_token())
+
+    def get_records(self):
+        extraction_url =  self.get_url_endpoint()
+        call_next, page_size = True, 1000
+        lookback_days = self.client.config["email_stats_lookback_days"]
+        since_date = pendulum.parse(self.client.config["start_date"]) \
+                             .in_timezone("UTC") \
+                             .add(days=-lookback_days)
+        until_date = pendulum.tomorrow().in_timezone("UTC")
+
+        params,headers = {"page":1,
+                         "per_page":page_size,
+                         "sort":"descending",
+                         "since":since_date.to_date_string(),
+                         "until":until_date.to_date_string()},{}
+        while call_next:
+            response =  self.client.get(extraction_url,params,headers,self.api_auth_version)
+            raw_records = response.get("records",[])
+            if not raw_records:
+                call_next =  False
+            params["page"]+=1
+            yield from raw_records
+
+    def sync(self,state,schema,stream_metadata,transformer):
+        with metrics.record_counter(self.tap_stream_id) as counter:
+            for record in self.get_records():
+                transformed_record = transformer.transform(record, schema, stream_metadata)
+                write_record(self.tap_stream_id, transformed_record)
+                counter.increment()
+        return state 
