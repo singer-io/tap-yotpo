3d4db9cd38bc2dec6afdaba8122380019a231580
VishalP <20889199+Vi6hal@users.noreply.github.com>
2022-08-12 09:35:42 +0530

    updated setup.cfg and pyproject.toml

--- setup.py
+++ setup.py
@@ -12,12 +12,11 @@ setup(
     install_requires=[
         "singer-python==5.12.1",
         "requests==2.20.0",
-        "pendulum==2.1.2",
-    ],
+        ],
     extras_require={
-        'dev': [
-            'pylint',
-            'ipdb',
+        "dev": [
+            "pylint",
+            "ipdb",
         ]
     },
     entry_points="""
@@ -25,8 +24,6 @@ setup(
     tap-yotpo=tap_yotpo:main
     """,
     packages=["tap_yotpo"],
-    package_data = {
-        "schemas": ["tap_yotpo/schemas/*.json"]
-    },
+    package_data={"schemas": ["tap_yotpo/schemas/*.json"]},
     include_package_data=True,
 )
