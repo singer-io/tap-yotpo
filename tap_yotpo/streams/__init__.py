ba81a713c9671b7b9378ba904adbca368b98efd9
Sourabh Gandhi <sgandhi@talend.com>
2022-08-30 13:20:01 +0530

    Order fulfillments stream changes - Added pagination logic, bookmarking and schema.

--- tap_yotpo/streams/__init__.py
+++ tap_yotpo/streams/__init__.py
@@ -8,15 +8,17 @@ from .unsubscribers import Unsubscribers
 from .collections import Collections
 from .customers import Customers
 from .product_variants import ProductVariants
+from .order_fulfillments import OrderFulfillments
 
 STREAMS = {
     Emails.tap_stream_id: Emails,
     Orders.tap_stream_id: Orders,
+    OrderFulfillments.tap_stream_id: OrderFulfillments,
     Products.tap_stream_id: Products,
     ProductReviews.tap_stream_id: ProductReviews,
+    ProductVariants.tap_stream_id: ProductVariants,
     Reviews.tap_stream_id: Reviews,
     Unsubscribers.tap_stream_id: Unsubscribers,
     Collections.tap_stream_id: Collections,
-    Customers.tap_stream_id: Customers,
-    ProductVariants.tap_stream_id: ProductVariants
+    Customers.tap_stream_id: Customers
 }
