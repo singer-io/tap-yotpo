16a70ddcb01be65627f63cfac1ca03b0ed7841f8
Vishal <vpachpinde@talend.com>
2022-09-01 14:00:05 +0530

    Support for orders stream (#42)
    * Changes:
    - added orders stream with incremental sync
    - `customer` key added to `orders` schema
    - added prefetch function

--- tap_yotpo/streams/__init__.py
+++ tap_yotpo/streams/__init__.py
@@ -1,5 +1,6 @@
 """tap-yotpo streams module."""
 from .emails import Emails
+from .orders import Orders
 from .product_reviews import ProductReviews
 from .products import Products
 from .reviews import Reviews
@@ -7,6 +8,7 @@ from .unsubscribers import Unsubscribers
 
 STREAMS = {
     Emails.tap_stream_id: Emails,
+    Orders.tap_stream_id: Orders,
     Products.tap_stream_id: Products,
     ProductReviews.tap_stream_id: ProductReviews,
     Reviews.tap_stream_id: Reviews,
---
+++ tap_yotpo/streams/orders.py
@@ -0,0 +1,86 @@
+"""tap-yotpo Orders stream module."""
+from typing import Dict, Iterator, List
+
+from singer import Transformer, get_logger, metrics, write_record
+from singer.utils import strftime, strptime_to_utc
+
+from ..helpers import ApiSpec
+from .abstracts import IncremetalStream, UrlEndpointMixin
+
+LOGGER = get_logger()
+
+
+class Orders(IncremetalStream, UrlEndpointMixin):
+    """class for Orders stream."""
+
+    stream = "orders"
+    tap_stream_id = "orders"
+    key_properties = ["yotpo_id"]
+    replication_key = "updated_at"
+    valid_replication_keys = ["updated_at"]
+    api_auth_version = ApiSpec.API_V3
+    config_start_key = "start_date"
+    url_endpoint = "https://api.yotpo.com/core/v3/stores/APP_KEY/orders"
+
+    def get_records(self) -> Iterator[Dict]:
+        """performs api querying and pagination of response."""
+        extraction_url = self.get_url_endpoint()
+        page_count, params = 1, {}
+        while True:
+            LOGGER.info("Calling Page %s", page_count)
+            response = self.client.get(extraction_url, params, {}, self.api_auth_version)
+
+            # retrive records from response.Orders key
+            raw_records = response.get(self.stream, [])
+
+            # retrive pagination from response.pagination.next_page_info key
+            next_param = response.get("pagination", {}).get("next_page_info", None)
+
+            if not raw_records:
+                LOGGER.warning("No records found on Page %s", page_count)
+                break
+
+            params["page_info"] = next_param
+            page_count += 1
+            yield from raw_records
+            if not next_param:
+                break
+
+    def sync(self, state: Dict, schema: Dict, stream_metadata: Dict, transformer: Transformer) -> Dict:
+        """Sync implementation for `product_reviews` stream."""
+        current_bookmark_date = self.get_bookmark(state)
+        max_bookmark = current_bookmark_date_utc = strptime_to_utc(current_bookmark_date)
+
+        with metrics.record_counter(self.tap_stream_id) as counter:
+            for record in self.get_records():
+                try:
+                    record_timestamp = strptime_to_utc(record[self.replication_key])
+                except IndexError as _:
+                    LOGGER.error("Unable to process Record, Exception occured: %s for stream %s", _, self.__class__)
+                    continue
+                if record_timestamp >= current_bookmark_date_utc:
+                    transformed_record = transformer.transform(record, schema, stream_metadata)
+                    write_record(self.tap_stream_id, transformed_record)
+                    counter.increment()
+                    max_bookmark = max(max_bookmark, record_timestamp)
+                else:
+                    LOGGER.warning("Skipping Record Older than the timestamp")
+            state = self.write_bookmark(state, value=strftime(max_bookmark))
+        return state
+
+    def prefetch_order_ids(self) -> List:
+        """Helper method implemented for other streams to load all order_ids.
+
+        eg: orders are required to fetch `fullfilment` stream
+        """
+        order_ids = getattr(self.client, "shared_order_ids", [])
+        if not order_ids:
+            LOGGER.info("Fetching all Order_ids")
+            for record in self.get_records():
+                try:
+                    order_ids.append((record["yotpo_id"], record["external_id"]))
+                except KeyError:
+                    LOGGER.warning("Unable to find external order ID or Yotpo ID")
+
+            self.client.shared_order_ids = sorted(order_ids, key=lambda _: _[0])
+        return order_ids
