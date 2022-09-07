1d7c678c38611e1e79653ecd83cf218d614c39b0
Sourabh Gandhi <sgandhi@talend.com>
2022-09-07 19:44:24 +0000

    Modified logger statements and optimized pending stream logic

--- tests/test_interrupted_sync.py
+++ tests/test_interrupted_sync.py
@@ -22,7 +22,6 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
         
         start_date = self.get_properties()['start_date'].replace('Z', '.000000Z')
         expected_streams = self.expected_streams()
-        # expected_streams = {'collections', 'emails', 'orders', 'order_fulfillments', 'products', 'product_variants', 'reviews', 'unsubscribers'}
         expected_replication_keys = self.expected_replication_keys()
         expected_replication_methods = self.expected_replication_method()
         LOGGER.info(f"expected_replication_keys = {expected_replication_keys} \n expected_replication_methods = {expected_replication_methods}")
@@ -47,19 +46,21 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
         first_sync_records = runner.get_records_from_target_output()
         first_sync_bookmarks = menagerie.get_state(conn_id)
         
-        LOGGER.info(f"first_sync_record_count = {first_sync_record_count} \n first_sync_bookmarks = {first_sync_bookmarks}")
-        LOGGER.info(f"first_sync_bookmarks = {first_sync_bookmarks} \n")
+        LOGGER.info(f"first_sync_record_count = {first_sync_record_count}")
+
         ##########################################################################
         # Update State Between Syncs
         ##########################################################################
         
         LOGGER.info("Current Bookmark after first sync: {}".format(first_sync_bookmarks))
         
-        completed_streams = {'collections', 'emails', 'orders', 'product_reviews'}
+        completed_streams = {'collections', 'emails', 'order_fulfillments', 'orders', 'product_reviews'}
         pending_streams = {'reviews'}
         interrupt_stream = 'product_variants'
         interrupted_sync_states = self.create_interrupt_sync_state(copy.deepcopy(first_sync_bookmarks), interrupt_stream, pending_streams, start_date)
         menagerie.set_state(conn_id, interrupted_sync_states)
+        
+        LOGGER.info(f"Interrupted Bookmark - {interrupted_sync_states}")
 
         ##########################################################################
         # Second Sync
@@ -69,7 +70,6 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
         second_sync_records = runner.get_records_from_target_output()
         second_sync_bookmarks = menagerie.get_state(conn_id)
         LOGGER.info(f"second_sync_record_count = {second_sync_record_count} \n second_sync_bookmarks = {second_sync_bookmarks}")
-        LOGGER.info(f"first_sync_records = {second_sync_records} \n")
         ##########################################################################
         # Test By Stream
         ##########################################################################
@@ -83,10 +83,6 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
                 # collect information for assertions from syncs 1 & 2 base on expected values
                 first_sync_count = first_sync_record_count.get(stream, 0)
                 second_sync_count = second_sync_record_count.get(stream, 0)
-                first_sync_messages = [record.get('data') for record in
-                                    first_sync_records.get(
-                                        stream, {}).get('messages', [])
-                                    if record.get('action') == 'upsert']
                 second_sync_messages = [record.get('data') for record in
                                         second_sync_records.get(
                                             stream, {}).get('messages', [])
@@ -100,52 +96,41 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
                     replication_key = next(iter(expected_replication_keys[stream]))
                     
                     interrupted_bookmark_value = interrupted_sync_states['bookmarks'][stream]
-                    LOGGER.info(f"Get properties = {self.get_properties()}")
                     if stream in completed_streams:
                         # Verify at least 1 record was replicated in the second sync
                         if stream in {'order_fulfillments', 'product_reviews', 'product_variants'}:
                             # Bookmarking logic differs compared to other streams.
                             self.assertGreaterEqual(second_sync_count,
                                             1, 
-                                            msg="Incorrect bookmarking for {}, at least one record should be replicated".format(stream))
+                                            msg="Incorrect bookmarking for {0}, at least one record should be replicated".format(stream))
                         elif second_bookmark_value[replication_key] == first_bookmark_value[replication_key]:
                             self.assertGreaterEqual(second_sync_count,
                                             1, 
-                                            msg="Incorrect bookmarking for {}, at least one record should be replicated".format(stream))
+                                            msg="Incorrect bookmarking for {0}, at least one record should be replicated".format(stream))
                         else:
                             # second sync bookmark is greater than first sync
                             self.assertGreater(second_sync_count,
                                                 1,
-                                                msg="Incorrect bookmarking for {}, more than one records should be replicated if second sync bookmark is greater than first sync".format(stream))
+                                                msg="Incorrect bookmarking for {0}, more than one records should be replicated if second sync bookmark is greater than first sync".format(stream))
 
                     elif stream == interrupted_sync_states.get('currently_syncing', None):
                         # For interrupted stream records sync count should be less equals
                         self.assertLessEqual(second_sync_count,
                                             first_sync_count,
-                                            msg="For interrupted stream, seconds sync record count should be lesser or equal to first sync".format(stream))
+                                            msg="For interrupted stream - {0}, seconds sync record count should be lesser or equal to first sync".format(stream))
 
                     elif stream in pending_streams:
                         # First sync and second sync record count match
-                        if stream in {'order_fulfillments', 'product_reviews', 'product_variants'}:
-                            # Bookmarking logic differs compared to other streams.
-                            self.assertGreaterEqual(second_sync_count,
-                                                    first_sync_count, 
-                                                    msg="For pending sync streams, second sync record count should be more than or equal to first sync".format(stream))
-                        elif second_bookmark_value[replication_key] == first_bookmark_value[replication_key]:
-                            self.assertEquals(second_sync_count,
-                                            first_sync_count,
-                                            msg="For pending sync stream, if bookmark values are same for first and second sync, record should match".format(stream))
-                        else:
-                            self.assertGreaterEqual(second_sync_count,
-                                                    first_sync_count,
-                                                    msg="For pending sync streams, second sync record count should be more than first sync".format(stream))     
+                        self.assertGreaterEqual(second_sync_count,
+                                                first_sync_count,
+                                                msg="For pending sync streams - {0}, second sync record count should be more than or equal to first sync".format(stream))     
 
                     else:
                         raise Exception("Invalid state of stream {0} in interrupted state, please update appropriate state for the stream".format(stream))
                     
                     if stream in {'product_reviews', 'product_variants', 'order_fulfillments'}:
                         if stream == 'product_reviews' :
-                            repl_key = 'domain_key'
+                            repl_key = 'product_yotpo_id'
                         elif stream == 'order_fulfillments':
                             repl_key = 'order_id' 
                         elif stream == 'product_variants':
@@ -154,12 +139,11 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
                             # Verify the second sync replication key value is Greater or Equal to the first sync bookmark for each id
                             replication_key_value = record.get(replication_key)
                             repl_key_id = str(record.get(repl_key))
-                            LOGGER.info(f"repl_key_id = {repl_key_id}")
                             interrupt_bookmark_value = interrupted_bookmark_value[repl_key_id] if repl_key_id in interrupted_bookmark_value.keys() else start_date
 
                             self.assertLessEqual(interrupt_bookmark_value,
                                                 replication_key_value,
-                                                msg="Second sync bookmark was set incorrectly, a record with a greater replication-key value was synced. Record = {}".format(record))
+                                                msg="Interrupt bookmark was set incorrectly, a record with a lesser replication-key value was synced compared to interrupt bookmark value. Record = {}".format(record))
                                                 # TODO: remove record from above message
 
                             # Verify the second sync bookmark value is the max replication key value for a given id
@@ -175,7 +159,7 @@ class YotpoInterruptedSyncTest(YotpoBaseTest):
 
                             self.assertLessEqual(interrupted_bookmark_value[replication_key],
                                                 replication_key_value,
-                                                msg="Second sync bookmark was set incorrectly, a record with a greater replication-key value was synced. Record = {}".format(record))
+                                                msg="Interrupt bookmark was set incorrectly, a record with a lesser replication-key value was synced compared to interrupt bookmark value. Record = {}".format(record))
                                                 # TODO: remove record from above message
 
                             # Verify the second sync bookmark value is the max replication key value for a given stream
