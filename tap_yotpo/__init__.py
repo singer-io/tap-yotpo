1a077eac2d26cb152d8e3e64d6e04e12bdf55b9b
VishalP <20889199+Vi6hal@users.noreply.github.com>
2022-09-15 13:00:32 +0530

    added 406 exception support

--- tap_yotpo/__init__.py
+++ tap_yotpo/__init__.py
@@ -1,16 +1,18 @@
 """tap-yotpo module."""
-import singer
-from singer import utils
+import sys
+
+from singer import get_logger, utils
+from singer.metrics import Counter, Timer
 
 from tap_yotpo.client import Client
 from tap_yotpo.discover import discover
 from tap_yotpo.sync import sync
 
 REQUIRED_CONFIG_KEYS = ["start_date", "api_key", "api_secret"]
-LOGGER = singer.get_logger()
+LOGGER = get_logger()
 
 
-@singer.utils.handle_top_exception(LOGGER)
+@utils.handle_top_exception(LOGGER)
 def main():
     """performs sync and discovery."""
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
@@ -18,7 +20,12 @@ def main():
     if args.discover:
         discover(args.config).dump()
     else:
-        sync(client, args.catalog or discover(args.config), args.state)
+        with Counter("total_requests", log_interval=sys.maxsize) as req_counter, Timer(
+            "total_extraction_time", None
+        ) as req_timer:
+            client.req_counter = req_counter
+            client.req_timer = req_timer
+            sync(client, args.catalog or discover(args.config), args.state)
 
 
 if __name__ == "__main__":
