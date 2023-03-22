453977df6758e78812283d4f1bb562327710ec90
Bryant Gray <bgray@talend.com>
2023-03-22 17:50:19 +0000

    Initite log loop

--- tap_yotpo/__init__.py
+++ tap_yotpo/__init__.py
@@ -15,17 +15,19 @@ LOGGER = get_logger()
 @utils.handle_top_exception(LOGGER)
 def main():
     """performs sync and discovery."""
-    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
-    client = Client(args.config)
-    if args.discover:
-        discover(args.config).dump()
-    else:
-        with Counter("total_requests", log_interval=sys.maxsize) as req_counter, Timer(
-            "total_extraction_time", None
-        ) as req_timer:
-            client.req_counter = req_counter
-            client.req_timer = req_timer
-            sync(client, args.catalog or discover(args.config), args.state)
+    while True:
+        LOGGER.info("testing log size")
+    # args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    # client = Client(args.config)
+    # if args.discover:
+    #     discover(args.config).dump()
+    # else:
+    #     with Counter("total_requests", log_interval=sys.maxsize) as req_counter, Timer(
+    #         "total_extraction_time", None
+    #     ) as req_timer:
+    #         client.req_counter = req_counter
+    #         client.req_timer = req_timer
+    #         sync(client, args.catalog or discover(args.config), args.state)
 
 
 if __name__ == "__main__":
