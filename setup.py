e4b97e1f8fae57deaee120e0135d8bd517916101
Andy Lu <andy@stitchdata.com>
2025-07-16 15:37:55 -0400

    Bump versions (#61)
    * Bump versions
    
    Co-authored-by: Ben Allred <skuttleman@gmail.com>
    
    * Bump tap version and update changelog (#62)
    
    ---------
    
    Co-authored-by: Ben Allred <skuttleman@gmail.com>
    Co-authored-by: Ben Allred <ben.allred@qlik.com>

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-yotpo",
-    version="2.0.4",
+    version="2.0.5",
     description="Singer.io tap for extracting data from the Yotpo API",
     author="Stitch",
     url="https://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_yotpo"],
     install_requires=[
-        "singer-python==5.13.0",
-        "requests==2.32.3",
+        "singer-python==5.13.2",
+        "requests==2.32.4",
     ],
     extras_require={
         "dev": [
