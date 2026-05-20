15f6adffd4b5a62c531ddb4559c5cac77b125800
Tushar Mittal <tushar.mittal@qlik.com>
2026-05-20 10:22:43 +0530

    requests library version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     py_modules=["tap_yotpo"],
     install_requires=[
         "singer-python==6.8.0",
-        "requests==2.34.0",
+        "requests==2.34.2",
     ],
     extras_require={
         "dev": [
